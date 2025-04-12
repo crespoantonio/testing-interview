a5058a188ba06e6cc6dd1cef4f0bc00fca2b6205
Antonio Crespo <95352915+crespoantonio@users.noreply.github.com>
2025-04-11 22:15:42 -0300

    Update cypress.js

--- UI/cypress.js
+++ UI/cypress.js
@@ -2,8 +2,8 @@ describe('Login Page Test', () => {
     it('should log in and land on dashboard', () => {
       cy.visit('www.example.com/login');
   
-      cy.get('username').type('admin');
-      cy.get('password').type('password123');
+      cy.get('username').write('admin');
+      cy.get('password').write('password123');
       cy.get('loginButton').click();
   
       cy.get('dashbord', { timeout: 10000 }).expect('visible');
