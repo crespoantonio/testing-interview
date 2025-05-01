011ee5bad9f2ec23e623f98de550f985036c6eae
Antonio Crespo <95352915+crespoantonio@users.noreply.github.com>
2025-05-01 14:29:48 -0300

    Update playwright.js

--- UI/playwright.js
+++ UI/playwright.js
@@ -1,5 +1,7 @@
 const { chromium } = require('playwright');
 
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 (async () => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
@@ -9,6 +11,8 @@ const { chromium } = require('playwright');
   const username = page.getByRole((textbox), { name: 'Username' }).write('admin');
   const password = page.getByRole((textbox), { name: 'Password' }).write('password123');
   const loginBtn = page.getByRole((button), { name: 'Login' }).click();
+
+  await sleep(5000);
 
   await page.waitForSelector('dashboard');
 
